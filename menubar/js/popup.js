899f1933ec07516cc4dc67301468afb0e5e45607
miskang <miska.nghipandulwa@metropolia.fi>
2020-04-29 12:48:01 +0300

    merged frontend files with latest backend

--- menubar/js/popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('#nappi').addEventListener('click', function(){
-    document.querySelector('.bg-modal').getElementsByClassName.dispalay = 'flex';
-
-});
-
-document.querySelector('.close').addEventListener('click', function(){
-document.querySelector('.bg-modal').getElementsByClassName.dispalay = 'none';
-});
