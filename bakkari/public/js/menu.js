2585069371b17315f3dc04be9b27a398b88cea35
teemumsa <teemu.salo4@metropolia.fi>
2020-05-01 18:38:31 +0300

    kuva kortit ja muuta skeidaa

--- bakkari/public/js/menu.js
+++ bakkari/public/js/menu.js
@@ -24,41 +24,76 @@ const themeMap = {
   }
   
   document.getElementById('themeButton').onclick = toggleTheme;
-  let log = document.getElementById('loggaus')
-  log.addEventListener('click', event =>  {
-    let loginbox = document.querySelector('.login-box');
-    let registerbox = document.querySelector('.register-box');
-   if(loginbox.style.display = "block"){
-     registerbox.style.display = "none";
-   }
+//   let log = document.getElementById('loggaus')
+//   log.addEventListener('click', event =>  {
+//     let loginbox = document.querySelector('.login-box');
+//     let registerbox = document.querySelector('.register-box');
+//    if(loginbox.style.display = "block"){
+//      registerbox.style.display = "none";
+//    }
 
-});
+// });
 
 
-  let reg = document.getElementById('reg')
-  reg.addEventListener('click', event =>  {
-    let loginbox = document.querySelector('.login-box');
-    let registerbox = document.querySelector('.register-box');
-    if(registerbox.style.display = "block"){
-    loginbox.style.display = "none";
-    }
-   else{
-    loginbox.style.display = "block";
-   }
-});
+//   let reg = document.getElementById('reg')
+//   reg.addEventListener('click', event =>  {
+//     let loginbox = document.querySelector('.login-box');
+//     let registerbox = document.querySelector('.register-box');
+//     if(registerbox.style.display = "block"){
+//     loginbox.style.display = "none";
+//     }
+//    else{
+//     loginbox.style.display = "block";
+//    }
+// });
 
-addMemeForm.addEventListener('submit', async (evt) => {
-  evt.preventDefault();
-  const fd = new FormData(addMemeForm);
-  const fetchOptions = {
-    method: 'POST',
-    body: fd,
-  };
-  const response = await fetch(url + '/meme', fetchOptions);
-  const json = await response.json();
-  console.log('add response', json);
-  getMeme();
-});
+// addMemeForm.addEventListener('submit', async (evt) => {
+//   evt.preventDefault();
+//   const fd = new FormData(addMemeForm);
+//   const fetchOptions = {
+//     method: 'POST',
+//     body: fd,
+//   };
+//   const response = await fetch(url + '/meme', fetchOptions);
+//   const json = await response.json();
+//   console.log('add response', json);
+//   getMeme();
+// });
+
+
+// function openMenu() {
+//   let koko = 200 
+//   document.getElementById('paska').style.marginLeft = `${koko}px`;
+//   document.getElementById('paska').style.transitionDuration = "600ms";
+  
+// }
+// function closeMenu() {
+//   let koko = 200
+  
+//   document.getElementById('paska').style.marginRight = `${koko}px`;
+//   document.getElementById('paska').style.transitionDuration = "600ms";
+//   }
+let menu = document.getElementById('menu');
+  menu.addEventListener("mouseover", event => {
+    let koko = 200 
+    console.log("paska")
+    let paska = document.getElementById('paska') 
+    
+    paska.style.marginLeft = `${koko}px`;
+    paska.style.transitionDuration = "600ms";
+    
+   });
+   menu.addEventListener("mouseout", event => {
+    let koko = -0 
+    console.log("paska")
+    let paska = document.getElementById('paska') 
+    
+    paska.style.marginLeft = `${koko}px`;
+    paska.style.transitionDuration = "600ms";
+    
+   });
+
+
 
 
 
