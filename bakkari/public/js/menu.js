406ef83ed39338b95354ce58b7c85f1ce5a3c8ec
teemumsa <teemu.salo4@metropolia.fi>
2020-05-02 13:24:56 +0300

    uusin

--- bakkari/public/js/menu.js
+++ bakkari/public/js/menu.js
@@ -24,55 +24,55 @@ const themeMap = {
   }
   
   document.getElementById('themeButton').onclick = toggleTheme;
-//   let log = document.getElementById('loggaus')
-//   log.addEventListener('click', event =>  {
-//     let loginbox = document.querySelector('.login-box');
-//     let registerbox = document.querySelector('.register-box');
-//    if(loginbox.style.display = "block"){
-//      registerbox.style.display = "none";
-//    }
+  let log = document.getElementById('loggaus')
+  log.addEventListener('click', event =>  {
+    let loginbox = document.querySelector('.login-box');
+    let registerbox = document.querySelector('.register-box');
+   if(loginbox.style.display = "block"){
+     registerbox.style.display = "none";
+   }
 
-// });
-
-
-//   let reg = document.getElementById('reg')
-//   reg.addEventListener('click', event =>  {
-//     let loginbox = document.querySelector('.login-box');
-//     let registerbox = document.querySelector('.register-box');
-//     if(registerbox.style.display = "block"){
-//     loginbox.style.display = "none";
-//     }
-//    else{
-//     loginbox.style.display = "block";
-//    }
-// });
-
-// addMemeForm.addEventListener('submit', async (evt) => {
-//   evt.preventDefault();
-//   const fd = new FormData(addMemeForm);
-//   const fetchOptions = {
-//     method: 'POST',
-//     body: fd,
-//   };
-//   const response = await fetch(url + '/meme', fetchOptions);
-//   const json = await response.json();
-//   console.log('add response', json);
-//   getMeme();
-// });
+});
 
 
-// function openMenu() {
-//   let koko = 200 
-//   document.getElementById('paska').style.marginLeft = `${koko}px`;
-//   document.getElementById('paska').style.transitionDuration = "600ms";
+  let reg = document.getElementById('reg')
+  reg.addEventListener('click', event =>  {
+    let loginbox = document.querySelector('.login-box');
+    let registerbox = document.querySelector('.register-box');
+    if(registerbox.style.display = "block"){
+    loginbox.style.display = "none";
+    }
+   else{
+    loginbox.style.display = "block";
+   }
+});
+
+addMemeForm.addEventListener('submit', async (evt) => {
+  evt.preventDefault();
+  const fd = new FormData(addMemeForm);
+  const fetchOptions = {
+    method: 'POST',
+    body: fd,
+  };
+  const response = await fetch(url + '/meme', fetchOptions);
+  const json = await response.json();
+  console.log('add response', json);
+  getMeme();
+});
+
+
+function openMenu() {
+  let koko = 200 
+  document.getElementById('paska').style.marginLeft = `${koko}px`;
+  document.getElementById('paska').style.transitionDuration = "600ms";
   
-// }
-// function closeMenu() {
-//   let koko = 200
+}
+function closeMenu() {
+  let koko = 200
   
-//   document.getElementById('paska').style.marginRight = `${koko}px`;
-//   document.getElementById('paska').style.transitionDuration = "600ms";
-//   }
+  document.getElementById('paska').style.marginRight = `${koko}px`;
+  document.getElementById('paska').style.transitionDuration = "600ms";
+  }
 let menu = document.getElementById('menu');
   menu.addEventListener("mouseover", event => {
     let koko = 200 
@@ -85,13 +85,43 @@ let menu = document.getElementById('menu');
    });
    menu.addEventListener("mouseout", event => {
     let koko = -0 
-    console.log("paska")
+    
     let paska = document.getElementById('paska') 
     
     paska.style.marginLeft = `${koko}px`;
     paska.style.transitionDuration = "600ms";
     
    });
+    let cardHolder = document.getElementById('cards')
+   function createCard (){
+    let cardDiv = document.createElement('div');
+    let cardImage = document.createElement('div');
+    let image = document.createElement('img');
+    let cardContent = document.createElement('div');
+    let title = document.createElement('p');
+    let cardInfo = document.createElement('div');
+    let title2 = document.createElement('p');
+    let like = document.createElement('p');
+
+    cardDiv.className = "card";
+    cardImage.className = "card-image";
+    cardContent.className = "card-content";
+    title.className = "card-title";
+    cardInfo.className = "card-info";
+    like.className = "card-like";
+
+    cardHolder.appendChild(cardDiv);
+    cardDiv.appendChild(cardImage);
+    cardImage.appendChild(image);
+    cardDiv.appendChild(cardContent);
+    cardContent.appendChild(title);
+    cardContent.appendChild(cardInfo);
+    cardInfo.appendChild(title2);
+    cardInfo.appendChild(like);
+
+    console.log("toimiiko")
+
+   };
 
 
 
