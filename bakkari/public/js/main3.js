f5f77e27be912daddefd00314ec9f6bec3c07966
Oliver <62246751+re-JECT-127@users.noreply.github.com>
2020-05-07 12:46:03 +0300

    Update main3.js

--- bakkari/public/js/main3.js
+++ bakkari/public/js/main3.js
@@ -1,5 +1,5 @@
 'use strict';
-const url = 'http://localhost:3000'; // change url when uploading to server
+const url = 'http://10.114.34.44/app/'; // change url when uploading to server
 
 // select existing html elements
 const loginWrapper = document.querySelector('#login-wrapper');
