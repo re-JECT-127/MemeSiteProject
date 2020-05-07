090910c72c0630ba8728d5491259e8fd1ebe6588
Oliver <62246751+re-JECT-127@users.noreply.github.com>
2020-05-07 12:45:36 +0300

    Update main.js

--- bakkari/public/js/main.js
+++ bakkari/public/js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-const url = 'http://localhost:3000'; // change url when uploading to server
+const url = 'http://10.114.34.44/app/'; // change url when uploading to server
 
 const ul = document.querySelector('ul');
 
